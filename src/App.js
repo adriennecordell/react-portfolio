bd6b0d6e9ec596e963c51476b6cdc8e14ca9fdb3
adriennecordell <adrienne.cordellac@gmail.com>
2023-02-06 14:49:02 -0600

    reformatted app.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Page from "./components/Page";
 import Footer from "./components/Footer";
 
 function App() {
-  const [pages] = useState([
-    {
-      name: "about me"
-    },
-    { name: "portfolio" },
-    { name: "contact" },
-    {
-      name: "resume"
-    }
-  ]);
-
-  const [currentPage, setCurrentPage] = useState(pages[0]);
-
-  return (
-    <div>
-      <Header>
-        <Nav
-          pages={pages}
-          setCurrentPage={setCurrentPage}
-          currentPage={currentPage}
-        ></Nav>
-      </Header>
-      <main>
-        <Page currentPage={currentPage}></Page>
-      </main>
-      <Footer />
-    </div>
-  );
+  <>
+  <Nav></Nav>
+  <Header></Header>
+  <Page></Page>
+  <Footer></Footer>
+  </>
+  
 }
+     
 
 export default App;
